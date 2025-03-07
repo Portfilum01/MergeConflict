4ef0fb6418b5db6c17a3a2b69299d0661122662b
Portfilum01 <sabastiandust@gmail.com>
2025-03-07 18:45:54 +1030

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Heres some text right here" << endl;
+	cout << "Heres a little more text just here" << endl;
+	cout << "I'm saying unecessary things" << endl;
+	cout << "Blah blah blah" << endl;
+	cout << "Billy flowers :)" << endl;
+}
